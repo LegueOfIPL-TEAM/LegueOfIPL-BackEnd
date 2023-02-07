8a14db2003c8ca4fd196fbadebd2dd88ca5545a2
alsrn6040@kakao.com <alsrn6040@kakao.com>
2023-02-07 21:35:55 +0900

    [feat] 직접 post 쏘기

--- src/crawling/crawling.service.ts
+++ src/crawling/crawling.service.ts
@@ -1,10 +1,9 @@
 import { HttpException, Injectable } from '@nestjs/common';
-// import {Builder, By, Key, until, Browser, CLASSNAME} from 'selenium-webdriver'
-const {Builder, By, Key, until, time} = require('selenium-webdriver');
 import { HttpService } from '@nestjs/axios'
-import * as cheerio from 'cheerio'
 import { matchData } from 'src/commons/dto/cawling.dto';
 import { chromium } from 'playwright'
+import * as cheerio from 'cheerio'
+
 
 
 @Injectable()
@@ -25,60 +24,52 @@ export class CrawlingService {
       
         // Load the page
         await page.goto("https://barracks.sa.nexon.com/clan/tjrbdlf121122112/clanMatch");
-        // await page.waitForSelector("#clanMatch > div.grid > div.gd-1.grid-width-x3 > div.histories > div:nth-child(1) > div.accordion > a")
         
-        // Find the hidden element
         const toggleElements = await page.$$(".accordion-toggle");
 
         for (const toggleButton of toggleElements) {
             await toggleButton.click();
-          }
-
-        // Make the hidden element visible
-        // const element = await page.locator('#clanMatch > div.grid > div.gd-1.grid-width-x3 > div.histories > div:nth-child(1) > div.accordion-detail').evaluate(element => element.style.display = 'block');
-        
-        // Get the HTML content of the page
+        }
+                
         const html = await page.content();
 
-    
-        // Load the HTML into Cheerio
         const $ = cheerio.load(html);
-    
-        // Extract the data from the visible element
-        // const accordionDetail = $(`.accordion-detail`)
         
-        
-        // const listItems = await page.$$(
-        //     `#clanMatch > div.grid > div.gd-1.grid-width-x3 > div.histories > div:nth-child(1) > div.accordion-detail > div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody`,
-        // );
-
-        // for (const item of listItems) {
-        //     const text = await item.evaluate(node => 
-        //         console.log(node.getAttributeNode('.first.highlight'))
-        //     );
-        //     console.log(text);
-        // }
-        
-        const data = $( `.tbody`)
+        const data = $( `.accordion-detail`)
     
         let array = [];
-
-        // #clanMatch > div.grid > div.gd-1.grid-width-x3 > div.histories > div:nth-child(2) > div.accordion-detail > div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr.first.highlight > td:nth-child(3)
-        // #clanMatch > div.grid > div.gd-1.grid-width-x3 > div.histories > div:nth-child(2) > div.accordion-detail > div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(6) > td:nth-child(3)
         
         data.each((idx, node) => {
-            
-            // console.log($(node).text())
-            const test = $(node).find('.data-v-56346afc').text()
-            
-
+        
             array.push({
-                "test": [ test ]
+                "redClanName": $(node).find('div > div > div.result > div.result-ribbon.red > div.name.text-white.clan').text(),
+                "blueClanName": $(node).find('div > div > div.result > div.result-ribbon.blue > div.name.text-white.clan').text(),
+                "score": $(node).find('div > div > div.result > div.result-score > div.boundary > span.value.text-dark.Rajdhani').text(),
+                "loseTeamInfo" : [
+                    {
+                        "nickName": $(node).find('div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(1) > td:nth-child(3) > div > a').text(),
+                        "damage": $(node).find('div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(1) > td:nth-child(9)').text(),
+                        "kill": $(node).find('div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(1) > td:nth-child(4)').text(),
+                    }
+                ]
+                
             })
         })
+
+        
+        
+        //             $(node).find('div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(2) > td:nth-child(1) > div > a').text(),
+        //             $(node).find('div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(3) > td:nth-child(1) > div > a').text(),
+        //             $(node).find('div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(4) > td:nth-child(1) > div > a').text(),
+        //             $(node).find('div > div > div.history-tab > div.tabs-details > div:nth-child(1) > div > div.tbody > table > tbody > tr:nth-child(5) > td:nth-child(1) > div > a').text()
         console.log(array)
+        
+        array.map((element) => {
+            console.log(element.loseTeamInfo)
+        })
         await browser.close();
     } 
+    
       
     filteringClanInIpl(dataArray: Array<matchData>){
         const nameOfIplClan = [
@@ -87,4 +78,44 @@ export class CrawlingService {
         '<raiser>', 'sweetie', 'hypeus', 'Relive', 'everything', '♡idyllic', 'recent.wct', '〃veritas', 'Critisism', 'massacre;', '#Serious', 'none+'
         ]
     }
+
+    // anotherWay(){
+
+        // let specificUrls = []
+          
+        // page.on('request', request => {
+        //   if (request.url().includes('https://barracks.sa.nexon.com/api/Match/GetMatchClanDetail/')) {
+        //     const urls = request.url()
+        //     specificUrls.push(urls)
+        //   }
+        // });  
+
+    //     const saData = await page.evaluate(async () => {
+    //         const response = await fetch('https://barracks.sa.nexon.com/api/Match/GetMatchClanDetail/230207180516124001/C/tjrbdlf121122112', {
+    //           method: 'POST',
+    //           headers: {
+    //             'Content-Type': 'application/json'
+    //           },
+    //         });
+    //         return await response.json();
+    //       });
+          
+    //       const { redUserList, matchResultDataInfo , blueUserList } = saData
+
+    //       const { blue_clan_name:blueClanName, blue_result:blueResult, lose_team_name:loseTeamName, red_result:redResult, match_time:matchTime  } = matchResultDataInfo
+
+    //       blueUserList.forEach((element) => {
+    //         console.log(element.nickname)
+    //       })
+
+    //       const testData = {
+    //         matchTime,
+    //         blueClanName,
+    //         blueResult,
+    //         blueUserList,
+    //         loseTeamName,
+    //         redResult,
+    //         redUserList,
+    //       }
+    // }
 }
